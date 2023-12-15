91c16e91af1b7455705af7af36988e5267ac0c23
nielsero <niels.cpp@gmail.com>
2023-12-15 18:17:25 +0200

    test: Create user with valid data

---
+++ tests/user/services/create-user.service.test.ts
@@ -0,0 +1,39 @@
+import { UserRepository } from "@/modules/user/protocols";
+import { InMemoryUserRepository } from "@/modules/user/providers/repositories";
+import { CreateUserService } from "@/modules/user/services";
+
+type SutTypes = {
+  sut: CreateUserService;
+  repository: UserRepository;
+};
+
+const makeSut = (): SutTypes => {
+  const repository = new InMemoryUserRepository();
+  const sut = new CreateUserService(repository);
+  return { sut, repository };
+};
+
+describe("CreateUserService", () => {
+  it("Should create user and add it to the database", async () => {
+    const { sut, repository } = makeSut();
+
+    const request = {
+      name: "John Doe",
+      email: "john.doe@gmail.com",
+      password: "password",
+    };
+
+    await sut.execute(request);
+
+    const user = await repository.findByEmail(request.email);
+
+    const expectedUser = {
+      id: expect.any(String),
+      name: request.name,
+      email: request.email,
+      password: request.password,
+    };
+
+    expect(user).resolves.toEqual(expectedUser);
+  });
+});
