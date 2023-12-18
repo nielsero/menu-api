e14cac0fab3195b0417cde657b326acdb840b35b
nielsero <niels.cpp@gmail.com>
2023-12-18 22:30:30 +0200

    feat: Add menu item elements to factory

--- src/factories/menu-item.factory.ts
+++ src/factories/menu-item.factory.ts
@@ -1,23 +1,65 @@
 import { MenuItemRepository, MenuRepository } from "@/modules/menu/protocols";
 import { InMemoryMenuItemRepository } from "@/modules/menu/providers/repositories";
-import { ZodAddMenuItemRequestValidator } from "@/modules/menu/providers/validators";
-import { AddMenuItemService } from "@/modules/menu/services";
+import {
+  ZodAddMenuItemRequestValidator,
+  ZodDeleteMenuItemRequestValidator,
+  ZodEditMenuItemRequestValidator,
+  ZodGetAllMenuItemsRequestValidator,
+  ZodGetAllPublishedMenuItemsRequestValidator,
+  ZodGetMenuItemRequestValidator,
+  ZodGetPublishedMenuItemRequestValidator,
+} from "@/modules/menu/providers/validators";
+import {
+  AddMenuItemService,
+  DeleteMenuItemService,
+  EditMenuItemService,
+  GetAllMenuItemsService,
+  GetAllPublishedMenuItemsService,
+  GetMenuItemService,
+  GetPublishedMenuItemService,
+} from "@/modules/menu/services";
 import { UserRepository } from "@/modules/user/protocols";
 import { makeMenu } from "@/factories/menu.factory";
 import { MenuItemRouter } from "@/modules/menu/routers";
-import { AddMenuItemController } from "@/modules/menu/controllers";
+import {
+  AddMenuItemController,
+  DeleteMenuItemController,
+  EditMenuItemController,
+  GetAllMenuItemsController,
+  GetAllPublishedMenuItemsController,
+  GetMenuItemController,
+  GetPublishedMenuItemController,
+} from "@/modules/menu/controllers";
 
 export type MenuItemTypes = {
   menuItemRouter: MenuItemRouter;
   addMenuItemController: AddMenuItemController;
   addMenuItemService: AddMenuItemService;
+  editMenuItemController: EditMenuItemController;
+  editMenuItemService: EditMenuItemService;
+  deleteMenuItemController: DeleteMenuItemController;
+  deleteMenuItemService: DeleteMenuItemService;
+  getAllMenuItemsController: GetAllMenuItemsController;
+  getAllMenuItemsService: GetAllMenuItemsService;
+  getMenuItemController: GetMenuItemController;
+  getMenuItemService: GetMenuItemService;
   menuItemRepository: MenuItemRepository;
+  getAllPublishedMenuItemsController: GetAllPublishedMenuItemsController;
+  getAllPublishedMenuItemsService: GetAllPublishedMenuItemsService;
+  getPublishedMenuItemController: GetPublishedMenuItemController;
+  getPublishedMenuItemService: GetPublishedMenuItemService;
   menuRepository: MenuRepository;
   userRepository: UserRepository;
 };
 
 const inMemoryMenuItemRepository = new InMemoryMenuItemRepository();
 const addMenuItemRequestValidator = new ZodAddMenuItemRequestValidator();
+const editMenuItemRequestValidator = new ZodEditMenuItemRequestValidator();
+const deleteMenuItemRequestValidator = new ZodDeleteMenuItemRequestValidator();
+const getAllMenuItemsRequestValidator = new ZodGetAllMenuItemsRequestValidator();
+const getAllPublishedMenuItemsRequestValidator = new ZodGetAllPublishedMenuItemsRequestValidator();
+const getMenuItemRequestValidator = new ZodGetMenuItemRequestValidator();
+const getPublishedMenuItemRequestValidator = new ZodGetPublishedMenuItemRequestValidator();
 
 export const makeMenuItem = (): MenuItemTypes => {
   const { userRepository, menuRepository } = makeMenu();
@@ -27,11 +69,70 @@ export const makeMenuItem = (): MenuItemTypes => {
     menuItemRepository: inMemoryMenuItemRepository,
   });
   const addMenuItemController = new AddMenuItemController(addMenuItemService);
-  const menuItemRouter = new MenuItemRouter({ add: addMenuItemController });
+  const editMenuItemService = new EditMenuItemService({
+    menuRepository,
+    requestValidator: editMenuItemRequestValidator,
+    menuItemRepository: inMemoryMenuItemRepository,
+  });
+  const editMenuItemController = new EditMenuItemController(editMenuItemService);
+  const deleteMenuItemService = new DeleteMenuItemService({
+    menuRepository,
+    requestValidator: deleteMenuItemRequestValidator,
+    menuItemRepository: inMemoryMenuItemRepository,
+  });
+  const deleteMenuItemController = new DeleteMenuItemController(deleteMenuItemService);
+  const getAllMenuItemsService = new GetAllMenuItemsService({
+    menuRepository,
+    requestValidator: getAllMenuItemsRequestValidator,
+    menuItemRepository: inMemoryMenuItemRepository,
+  });
+  const getAllMenuItemsController = new GetAllMenuItemsController(getAllMenuItemsService);
+  const getMenuItemService = new GetMenuItemService({
+    menuRepository,
+    requestValidator: getMenuItemRequestValidator,
+    menuItemRepository: inMemoryMenuItemRepository,
+  });
+  const getMenuItemController = new GetMenuItemController(getMenuItemService);
+  const getAllPublishedMenuItemsService = new GetAllPublishedMenuItemsService({
+    menuRepository,
+    requestValidator: getAllPublishedMenuItemsRequestValidator,
+    menuItemRepository: inMemoryMenuItemRepository,
+  });
+  const getAllPublishedMenuItemsController = new GetAllPublishedMenuItemsController(
+    getAllPublishedMenuItemsService,
+  );
+  const getPublishedMenuItemService = new GetPublishedMenuItemService({
+    menuRepository,
+    requestValidator: getPublishedMenuItemRequestValidator,
+    menuItemRepository: inMemoryMenuItemRepository,
+  });
+  const getPublishedMenuItemController = new GetPublishedMenuItemController(getPublishedMenuItemService);
+
+  const menuItemRouter = new MenuItemRouter({
+    addMenuItemController,
+    editMenuItemController,
+    deleteMenuItemController,
+    getAllMenuItemsController,
+    getMenuItemController,
+    getAllPublishedMenuItemsController,
+    getPublishedMenuItemController,
+  });
   return {
     menuItemRouter,
     addMenuItemController,
     addMenuItemService,
+    editMenuItemController,
+    editMenuItemService,
+    deleteMenuItemController,
+    deleteMenuItemService,
+    getAllMenuItemsController,
+    getAllMenuItemsService,
+    getMenuItemController,
+    getMenuItemService,
+    getAllPublishedMenuItemsController,
+    getAllPublishedMenuItemsService,
+    getPublishedMenuItemController,
+    getPublishedMenuItemService,
     menuRepository,
     userRepository,
     menuItemRepository: inMemoryMenuItemRepository,
--- src/modules/menu/services/get-all-published-menu-items.service.ts
+++ src/modules/menu/services/get-all-published-menu-items.service.ts
@@ -1,6 +1,7 @@
 import { MenuNotFound } from "@/shared/errors";
 import { MenuItem } from "../entities";
 import { MenuItemRepository, MenuRepository } from "../protocols";
+import { RequestValidator } from "@/shared/protocols";
 
 export type GetAllPublishedMenuItemsRequest = {
   menuId: string;
@@ -9,20 +10,24 @@ export type GetAllPublishedMenuItemsRequest = {
 export type GetAllPublishedMenuItemsResponse = MenuItem[];
 
 export type GetAllPublishedMenuItemsProviders = {
+  requestValidator: RequestValidator<GetAllPublishedMenuItemsRequest>;
   menuRepository: MenuRepository;
   menuItemRepository: MenuItemRepository;
 };
 
 export class GetAllPublishedMenuItemsService {
+  private readonly requestValidator: RequestValidator<GetAllPublishedMenuItemsRequest>;
   private readonly menuRepository: MenuRepository;
   private readonly menuItemRepository: MenuItemRepository;
 
   constructor(private readonly providers: GetAllPublishedMenuItemsProviders) {
+    this.requestValidator = providers.requestValidator;
     this.menuRepository = providers.menuRepository;
     this.menuItemRepository = providers.menuItemRepository;
   }
 
   async execute(request: GetAllPublishedMenuItemsRequest): Promise<GetAllPublishedMenuItemsResponse> {
+    await this.requestValidator.validate(request);
     const menus = await this.menuRepository.findAllPublished();
     const menu = menus.find((menu) => menu.id === request.menuId);
     if (!menu) throw new MenuNotFound();
