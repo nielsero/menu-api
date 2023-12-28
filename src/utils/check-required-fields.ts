1262924264659f559516d604cc8f47fc5d530f68
nielsero <niels.cpp@gmail.com>
2023-12-28 17:37:50 +0200

    refactor: Remove unused utils

--- src/utils/check-required-fields.ts
+++
@@ -1,8 +0,0 @@
-import { ValidationError } from "@/shared/errors";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const checkRequiredFields = (request: any, fields: string[]) => {
-  fields.forEach((field) => {
-    if (!request[field]) throw new ValidationError(`${field} is required`);
-  });
-};
